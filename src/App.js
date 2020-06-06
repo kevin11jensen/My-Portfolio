7654050e608a7ae4d637452f5d8b692b6547142f
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-06-06 10:08:54 -0700

    Created my first greensock animation!!!!

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-
+import gsap from 'gsap';
 import './App.css';
 
 import NavBar from './components/NavBar.js';
 
 
 export default function App() {
-
+  
+  function greenSock() {
+    gsap.to('h1', {duration: 2, x: 300});
+  }
+  
   return (
 
     <div>
 
       <NavBar />
 
-      <h1>Lets Learn Greensock!</h1>
+      <h1 onClick = {greenSock}>Lets Learn Greensock!</h1>
 
     </div>
   )
