607bbc1c1994016140bdf3d532c3c8ae7f8998b6
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-06-06 09:17:08 -0700

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function NavBar() {
+
+    return (
+
+        <div>
+
+            <nav>
+                Kevin11Jensen.com
+            </nav>
+    
+        </div>
+    )
+}
